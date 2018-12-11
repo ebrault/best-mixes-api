a698fe8e3f5ec343735013bae03f2687832d33be
ebrault <emmet.brault@gmail.com>
2018-12-11 12:37:33 -0500

    Privatized Mix Resource
    * Tested auth curl scripts successfully
    * Refactored mix controller to inherit from protected controller
    * Generated migration to add user id to mixes
    * Checked migration file for consistency
    * Ran db migrate
    * Checked schema for consistency
    * Added relations to users and mixes
    * Refactored mix curl scripts to require authentication
    * Tested all authenticated curl scripts successfully

--- app/models/mix.rb
+++ app/models/mix.rb
@@ -1,2 +1,3 @@
 class Mix < ApplicationRecord
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_165353) do
+ActiveRecord::Schema.define(version: 2018_12_11_172928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2018_12_11_165353) do
     t.string "comments"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_mixes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,4 +48,5 @@ ActiveRecord::Schema.define(version: 2018_12_11_165353) do
   end
 
   add_foreign_key "examples", "users"
+  add_foreign_key "mixes", "users"
 end
